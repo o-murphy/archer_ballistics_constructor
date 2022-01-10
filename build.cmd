c1ecd27e90ef2abecfda942aed64ca7178b59910
Murphy <thehelixpg@gmail.com>
2022-01-10 17:40:40 +0200

    append en, ua languages

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
-pyinstaller -y -w --clean -i=.rsrc\Icon.ico --version-file=version.txt --add-data=.rsrc;.rsrc\ --add-data=drv;drv\ archerbc.py
+pyinstaller -y -w --clean -i=.rsrc\Icon.ico --version-file=version.txt --add-data=.rsrc;.rsrc\ --add-data=translate;translate\ --add-data=qss;qss\ --add-data=drv;drv\ archerbc.py
 pyinstaller -y -w --clean -i=.rsrc\Icon.ico --version-file=uversion.txt archerbc_updater.py
 "%ProgramFiles(x86)%\INNOSE~1\ISCC.exe" /Qp installer64_relative.iss
 AcherBC_setup_x64.exe /verysilent
