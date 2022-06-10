3eed9fdd412c523f20e15088c4a813e9caefd319
Murphy <thehelixpg@gmail.com>
2022-06-10 13:11:09 +0300

    df editor update

--- gui/drag_func_editor/drag_func_edit.py
+++ gui/drag_func_editor/drag_func_edit.py
@@ -11,72 +11,11 @@ from modules import BConverter
 from modules import State, StateDidUpdate
 from modules.env_update import USER_RECENT
 
+from .defaults import DEFAULTS
+
 from ..stylesheet import load_qss
 
 rnd = BConverter.auto_rnd
-
-default_g1 = """
-0,0	0,263
-0,5	0,203
-0,6	0,203
-0,7	0,217
-0,8	0,255
-0,9	0,342
-0,95	0,408
-1,0	0,481
-1,05	0,543
-1,1	0,588
-1,2	0,639
-1,3	0,659
-1,4	0,663
-1,5	0,657
-1,6	0,647
-1,8	0,621
-2,0	0,593
-2,2	0,569
-2,5	0,54
-3,0	0,513
-3,5	0,504
-4,0	0,501
-"""
-
-DEFAULTS = {
-    'default_data': None,
-    'current_data': None,
-    'distances': None,
-    'current_distance': None,
-    'default_drop': None,
-    'current_drop': None,
-    'rifleName': 'Template',
-    'caliberName': '.308 Win',
-    'sh': 90,
-    'twist': 11,
-    'caliberShort': '',
-    'rightTwist': True,
-    'bulletName': 'Hornady ELD Match',
-    'weight': 178.0,
-    'length': 1.3,
-    'diameter': 0.308,
-    'weightTile': '178gr',
-    'drags': [],
-    'drag_idx': -1,
-    'cartridgeName': 'Template',
-    'mv': 800,
-    'temp': 15,
-    'ts': 1.55,
-    'z_pressure': 760,
-    'z_angle': 0,
-    'z_temp': 15,
-    'z_humidity': 50,
-    'z_azimuth': 270,
-    'z_latitude': 0,
-    'z_powder_temp': 15,
-    'z_d': 100,
-    'z_x': 0.0,
-    'z_y': 0.0,
-    'df_data': None,
-    'df_type': 'Custom'
-}
 
 
 class DragFuncEditDialog(QtWidgets.QDialog, Ui_DragFuncEditDialog):
