c40fe7c18449733378d3e971b850e0600773095d
Murphy <thehelixpg@gmail.com>
2022-06-23 13:24:07 +0300

    df editor update

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,5 +1,4 @@
-from typing import Callable, Iterator, Union, Optional, Iterable
-import math
+from typing import Union, Iterable
 
 
 class DragFunctions(object):
@@ -18,20 +17,33 @@ class DragFunctions(object):
         (4.800, 0.499), (5.000, 0.499)
     )
     # G7 = (
-    #     (0.000, 0.120), (0.050, 0.120), (0.100, 0.120), (0.150, 0.119), (0.200, 0.119), (0.250, 0.119), (0.300, 0.119),
-    #     (0.350, 0.119), (0.400, 0.119), (0.450, 0.119), (0.500, 0.119), (0.550, 0.119), (0.600, 0.119), (0.650, 0.120),
-    #     (0.700, 0.120), (0.725, 0.121), (0.750, 0.122), (0.775, 0.123), (0.800, 0.124), (0.825, 0.127), (0.850, 0.131),
-    #     (0.875, 0.137), (0.900, 0.146), (0.925, 0.166), (0.950, 0.205), (0.975, 0.299), (1.000, 0.380), (1.025, 0.402),
-    #     (1.050, 0.404), (1.075, 0.403), (1.100, 0.401), (1.125, 0.399), (1.150, 0.396), (1.200, 0.388), (1.250, 0.381),
-    #     (1.300, 0.373), (1.350, 0.366), (1.400, 0.358), (1.500, 0.344), (1.550, 0.338), (1.600, 0.332), (1.650, 0.326),
-    #     (1.700, 0.321), (1.750, 0.316), (1.800, 0.312), (1.850, 0.308), (1.900, 0.304), (1.950, 0.301), (2.000, 0.298),
-    #     (2.050, 0.295), (2.100, 0.292), (2.150, 0.289), (2.200, 0.286), (2.250, 0.283), (2.300, 0.281), (2.350, 0.278),
-    #     (2.400, 0.275), (2.450, 0.273), (2.500, 0.270), (2.550, 0.267), (2.600, 0.264), (2.650, 0.262), (2.700, 0.259),
-    #     (2.750, 0.256), (2.800, 0.253), (2.850, 0.251), (2.900, 0.248), (2.950, 0.245), (3.000, 0.242), (3.100, 0.237),
-    #     (3.200, 0.231), (3.300, 0.226), (3.400, 0.221), (3.500, 0.215), (3.600, 0.211), (3.700, 0.206), (3.800, 0.202),
-    #     (3.900, 0.198), (4.000, 0.194), (4.200, 0.186), (4.400, 0.179), (4.600, 0.173), (4.800, 0.167), (5.000, 0.162)
+    #     (0.000, 0.120), (0.050, 0.120), (0.100, 0.120), (0.150, 0.119), (0.200, 0.119), (0.250, 0.119),
+    #     (0.300, 0.119),
+    #     (0.350, 0.119), (0.400, 0.119), (0.450, 0.119), (0.500, 0.119), (0.550, 0.119), (0.600, 0.119),
+    #     (0.650, 0.120),
+    #     (0.700, 0.120), (0.725, 0.121), (0.750, 0.122), (0.775, 0.123), (0.800, 0.124), (0.825, 0.127),
+    #     (0.850, 0.131),
+    #     (0.875, 0.137), (0.900, 0.146), (0.925, 0.166), (0.950, 0.205), (0.975, 0.299), (1.000, 0.380),
+    #     (1.025, 0.402),
+    #     (1.050, 0.404), (1.075, 0.403), (1.100, 0.401), (1.125, 0.399), (1.150, 0.396), (1.200, 0.388),
+    #     (1.250, 0.381),
+    #     (1.300, 0.373), (1.350, 0.366), (1.400, 0.358), (1.500, 0.344), (1.550, 0.338), (1.600, 0.332),
+    #     (1.650, 0.326),
+    #     (1.700, 0.321), (1.750, 0.316), (1.800, 0.312), (1.850, 0.308), (1.900, 0.304), (1.950, 0.301),
+    #     (2.000, 0.298),
+    #     (2.050, 0.295), (2.100, 0.292), (2.150, 0.289), (2.200, 0.286), (2.250, 0.283), (2.300, 0.281),
+    #     (2.350, 0.278),
+    #     (2.400, 0.275), (2.450, 0.273), (2.500, 0.270), (2.550, 0.267), (2.600, 0.264), (2.650, 0.262),
+    #     (2.700, 0.259),
+    #     (2.750, 0.256), (2.800, 0.253), (2.850, 0.251), (2.900, 0.248), (2.950, 0.245), (3.000, 0.242),
+    #     (3.100, 0.237),
+    #     (3.200, 0.231), (3.300, 0.226), (3.400, 0.221), (3.500, 0.215), (3.600, 0.211), (3.700, 0.206),
+    #     (3.800, 0.202),
+    #     (3.900, 0.198), (4.000, 0.194), (4.200, 0.186), (4.400, 0.179), (4.600, 0.173), (4.800, 0.167),
+    #     (5.000, 0.162)
     # )
     #
+
     G7 = (
         (0.0, 0.11980000138282776), (0.05000000074505806, 0.11969999969005585),
         (0.10000000149011612, 0.11959999799728394), (0.15000000596046448, 0.11940000206232071),
@@ -71,14 +83,9 @@ class DragFunctions(object):
 
 
 class Constant(object):
-    # speed_of_sound = 340
     # R = 8.31446262
     # y = 7 / 5
     # tk0 = 273.15
-    # tc = 26
-    # tf = tc * 9 / 5 + 32
-    # pmmrtst = 760
-    # patm = pmmrtst / 760
     # M = 0.0289645
 
     # def get_speed_of_sound(self):
@@ -154,8 +161,8 @@ class Constant(object):
 
 class Calculator(object):
     def __init__(self, w: float = 90, d: float = .243, bc: Union[float, list[(float, int)]] = 0.218,
-                 df_type: Iterable[tuple[float, float]] = None,
-                 atmo: tuple[float, float, float] = None):
+                 df_type: tuple = None,
+                 atmo: tuple = None):
         self._width = w
         self._diameter = d
         self._bc = bc
@@ -202,11 +209,11 @@ class Calculator(object):
         self.calculate_df()
 
     @property
-    def df_type(self) -> Iterable[tuple[float, float]]:
+    def df_type(self) -> tuple:
         return self._df_type
 
     @df_type.setter
-    def df_type(self, value: Iterable[tuple[float, float]]):
+    def df_type(self, value: tuple):
         self._df_type = value
         self.calculate_df()
 
@@ -224,7 +231,10 @@ class Calculator(object):
             if isinstance(self._bc, float):
                 self.calculate_drag_function()
             elif isinstance(self._bc, list):
-                self.calculate_drag_function_multi_bc()
+                if len(self._bc) > 1:
+                    self.calculate_drag_function_multi_bc()
+                else:
+                    self._bc = self._bc[0][0]
 
     def sectional_density(self) -> float:
         """ params:
@@ -234,115 +244,55 @@ class Calculator(object):
         return self._width / (self._diameter ** 2)
 
     def form_factor_by_bullet_data(self, bc):
-        return round(self.sectional_density() / 7000 / bc, 4)
+        return self.sectional_density() / 7000 / bc
 
     @staticmethod
-    def counted_drag_coefficient(i, cd_def):
-        return round(cd_def * i, 4)
+    def counted_drag_coefficient(form_factor, cdst):
+        return cdst * form_factor
 
     def calculate_drag_function(self):
-        i = self.form_factor_by_bullet_data(self._bc, )
+        form_factor = self.form_factor_by_bullet_data(self._bc, )
         drag_function = []
-        for v, cd_def in self._df_type:
-            cd = self.counted_drag_coefficient(i, cd_def)
-            drag_function.append((v, cd))
+        for vst, cdst in self._df_type:
+            cd = self.counted_drag_coefficient(form_factor, cdst)
+            drag_function.append((vst, cd))
         self._df_data = drag_function
 
     def calculate_drag_function_multi_bc(self):
-        i_table = []
-        for bc, v in self._bc:
-            i_table.append((self.form_factor_by_bullet_data(bc), v))
+
+        bc_mah = [[i[0], i[1] / self.speed_of_sound] for i in self._bc]
+        bc_mah[0][1] = self._df_type[-1][0]
+        bc_mah.insert(len(bc_mah), [bc_mah[-1][0], self._df_type[0][0]])
+        bc_extended = [bc_mah[0][0], ]
+
+        for i in range(1, len(bc_mah)):
+            bc_max, v_max = bc_mah[i - 1]
+            bc_min, v_min = bc_mah[i]
+            df_part = list(filter(lambda point: v_max > point[0] >= v_min, self.df_type))
+            ddf = len(df_part)
+            bc_delta = (bc_max - bc_min) / ddf
+            for j in range(ddf):
+                bc_extended.append(bc_max - bc_delta * j)
 
         drag_function = []
-
-        for j, (vst, cdst) in enumerate(self._df_type):
-            v = vst * self._speed_of_sound
-            # for idx, (i, vm) in enumerate(i_table):
-
-            if i_table[2][1] > v >= 0:
-
-
-                cd = self.counted_drag_coefficient(i_table[2][0], cdst)
-                drag_function.append((vst, cd))
-
-            if i_table[1][1] > v >= i_table[2][1]:
-
-                i = i_table[1][0]
-
-                cd = self.counted_drag_coefficient(i, cdst)
-                drag_function.append((vst, cd))
-
-            if i_table[0][1] > v >= i_table[1][1]:
-
-                i = i_table[0][0]
-
-                cd = self.counted_drag_coefficient(i, cdst)
-                drag_function.append((vst, cd))
-
-            if v >= i_table[0][1]:
-                cd = self.counted_drag_coefficient(i_table[0][0], cdst)
-                drag_function.append((vst, cd))
-
-            #
-            #     coef = (self._df_type[j - 1][0] / vst)
-            #     ic = i_table[1][0] * coef
-            #
-            #     cd = self.counted_drag_coefficient(ic, cdst)
-            #     drag_function.append((vst, cd))
-
+        for i, (vst, cdst) in enumerate(self._df_type):
+            bc = bc_extended[len(bc_extended) - 1 - i]
+            form_factor = self.form_factor_by_bullet_data(bc)
+            cd = self.counted_drag_coefficient(form_factor, cdst)
+            drag_function.append((vst, cd))
         drag_function.sort()
-
         self._df_data = drag_function
-
-        # @staticmethod
-        # def bc_by_retardation(r_def: float, r: float) -> float:
-        #     """ params:
-        #         r_def: замедление стандартной пули
-        #         r: замедление тестируемой пули
-        #     """
-        #     return r_def / r
-
-        # @staticmethod
-        # def bc_by_form_factor(sd: float = None, w: float = None, d: float = None, i: float = None) -> float:
-        #     """ params:
-        #         sd: sectional density
-        #         w: weight in pounds
-        #         d: diameter in inches
-        #         i: drag-coefficient
-        #     """
-        #     if sd and i:
-        #         return sd / i
-        #     elif w and d and i:
-        #         return w / (i * d ** 2)
-        #     else:
-        #         raise ValueError(f'some arguments are wrong {locals()}')
-
-        # def retardation(self, **kwargs):
-        #     """ params:
-        #         r_def: замедление стандартной пули
-        #         r: замедление тестируемой пули, футы/сек2
-        #         v: скорость тестируемой пули, футы/сек
-        #         m: степень
-        #         A: константа
-        #         BC: баллистический коэффициент
-        #         p: сила сопротивления воздуха
-        #         g: гравитационная константа
-        #
-        #         r_def = a * v ** m = bc * r = pg
-        #         r = a * v ** m / bc
-        #     """
-        #
-        #     return
 
 
 if __name__ == '__main__':
     from PyQt5.QtWidgets import QApplication
     import sys
+
     app = QApplication(sys.argv)
     calculator = Calculator(w=178, d=0.308, bc=[
-        (0.275, 800),
-        (0.26, 700),
-        (0.255, 500)
+        (0.268, 800),
+        (0.255, 700),
+        (0.25, 500)
     ], df_type=DragFunctions.G7, atmo=(15, 760, 50))
 
     # calculator = Calculator(w=178, d=0.308, bc=0.268, df_type=DragFunctions.G7, atmo=(15, 760, 50))
