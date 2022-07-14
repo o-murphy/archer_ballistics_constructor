9302b875559af61f32732845088cc77b90973577
Murphy <thehelixpg@gmail.com>
2022-07-14 15:05:31 +0300

    started adaptation for py_ballisticcalc

--- calculator/balcalc.py
+++ calculator/balcalc.py
@@ -1,104 +1,114 @@
 from modules import State
-from py_ballisticcalc.extended.profile_extended import *
-from py_ballisticcalc.extended.multiple_ballistic_coefficient import MultipleBallisticCoefficient
+# from py_ballisticcalc.extended.profile_extended import *
+# from py_ballisticcalc.extended.multiple_ballistic_coefficient import MultipleBallisticCoefficient
+
+from py_ballisticcalc.lib.profile import Profile
+from py_ballisticcalc.lib.bmath.unit import *
+from py_ballisticcalc.lib.drag import DragTableG1, DragTableG7
+from py_ballisticcalc.lib.weapon import TwistRight, TwistLeft
+from py_ballisticcalc.lib.trajectory_calculator import TrajectoryData
+from py_ballisticcalc.lib.atmosphere import Atmosphere, IcaoAtmosphere
+
+import math
 
 
-class EditorProfile(ProfileExtended):
-
-    @property
-    def calculated_drag_function(self):
-        if self.trajectory_data:
-            return self._calculated_drag_function
-        return None
-
-    @property
-    def distance_step(self):
-        return self._distance_step
-
-    @property
-    def drag_table(self):
-        return self._drag_table
-
-    @property
-    def bullet_diameter(self):
-        return self._bullet_diameter
-
-    @property
-    def bullet_weight(self):
-        return self._bullet_weight
+# class EditorProfile(ProfileExtended):
+#
+#     @property
+#     def calculated_drag_function(self):
+#         if self.trajectory_data:
+#             return self._calculated_drag_function
+#         return None
+#
+#     @property
+#     def distance_step(self):
+#         return self._distance_step
+#
+#     @property
+#     def drag_table(self):
+#         return self._drag_table
+#
+#     @property
+#     def bullet_diameter(self):
+#         return self._bullet_diameter
+#
+#     @property
+#     def bullet_weight(self):
+#         return self._bullet_weight
 
 
 class DragEditorState(State):
     def __init__(self, widget, state: dict = None, **kwargs):
         super(DragEditorState, self).__init__(widget, state, **kwargs)
 
-        self.profile = EditorProfile(maximum_distance=(2500, unit.DistanceMeter),
-                                     distance_step=(1, unit.DistanceMeter),
-                                     sight_angle=(0, 0),
-                                     maximum_step_size=(5, unit.DistanceFoot)
-                                     )
+        # self.profile = EditorProfile(maximum_distance=(2500, unit.DistanceMeter),
+        #                              distance_step=(1, unit.DistanceMeter),
+        #                              sight_angle=(0, 0),
+        #                              maximum_step_size=(5, unit.DistanceFoot)
+        #                              )
+
+        self.profile = None
 
         self.mbc = None
 
         self.onStateUpdate.connect(self.calculate_trajectory)
 
     def setProfile(self):
-        self.profile._bullet_diameter = unit.Distance(self.diameter, unit.DistanceInch)
-        self.profile._bullet_weight = unit.Weight(self.weight, unit.WeightGrain)
-        self.profile._bullet_length = unit.Distance(self.length, unit.DistanceInch)
-        self.profile._muzzle_velocity = unit.Velocity(self.mv, unit.VelocityMPS)
-        self.profile._pressure = unit.Pressure(self.z_pressure, unit.PressureMmHg)
-        self.profile._temperature = unit.Temperature(self.z_temp, unit.TemperatureCelsius)
-        self.profile._humidity = self.z_humidity
-        self.profile._zero_distance = unit.Distance(self.z_d, unit.DistanceMeter)
-        self.profile._twist = unit.Distance(self.twist, unit.DistanceInch)
-        self.profile._twist_direction = 1 if self.rightTwist else 2
-        self.profile._sight_height = unit.Distance(self.sh, unit.DistanceMillimeter)
+
+        custom_df = []
+        mbc = []
+        bc_value = 0
+        drag_table = DragTableG1
 
         if self.df_type in ['G1', 'G1 Multi-BC']:
 
             if isinstance(self.df_data, float):
-                self.profile._drag_table = DragTableG1
-                self.profile._bc_value = self.df_data
+                drag_table = DragTableG1
+                bc_value = self.df_data
 
             elif isinstance(self.df_data, list):
-                self.mbc = MultipleBallisticCoefficient(
-                    self.df_data, unit.VelocityMPS,
-                    DragTableG1,
-                    self.profile.bullet_diameter,
-                    self.profile.bullet_weight
-                )
-                self.profile._drag_table = 0
-                self.profile._bc_value = 0
-                self.profile._custom_drag_function = self.mbc.calculate_custom_drag_func()
+                drag_table = DragTableG1
+                bc_value = 0
+                custom_df = []
+                mbc = self.mbc
 
-        elif self.df_type in ['G7', 'G1 Multi-BC']:
+        if self.df_type in ['G7', 'G7 Multi-BC']:
 
             if isinstance(self.df_data, float):
-                self.profile._drag_table = DragTableG7
-                self.profile._bc_value = self.df_data
+                drag_table = DragTableG7
+                bc_value = self.df_data
 
             elif isinstance(self.df_data, list):
-                self.mbc = MultipleBallisticCoefficient(
-                    self.df_data, unit.VelocityMPS,
-                    DragTableG7,
-                    self.profile.bullet_diameter,
-                    self.profile.bullet_weight
-                )
-                self.profile._drag_table = 0
-                self.profile._bc_value = 0
-                self.profile._custom_drag_function = self.mbc.calculate_custom_drag_func()
+                drag_table = DragTableG7
+                bc_value = 0
+                custom_df = []
+                mbc = self.mbc
 
-        elif self.df_type in ['Custom']:
-            self.profile._drag_table = 0
-            self.profile._bc_value = 0
-            self.profile._custom_drag_function = self.df_data
+        self.profile = Profile(
+            bc_value=bc_value,
+            custom_drag_function=custom_df,
+            multiple_bc_table=mbc,
+            maximum_distance=(2500, DistanceMeter),
+            distance_step=(1, DistanceMeter),
+            sight_angle=(0, 0),
+            maximum_step_size=(5, DistanceFoot),
+            bullet_diameter=(self.diameter, DistanceInch),
+            bullet_weight=(self.weight, WeightGrain),
+            bullet_length=(self.length, DistanceInch),
+            muzzle_velocity=(self.mv, VelocityMPS),
+            pressure=(self.z_pressure, PressureMmHg),
+            temperature=(self.z_temp, TemperatureCelsius),
+            humidity=self.z_humidity,
+            zero_distance=(self.z_d, DistanceMeter),
+            twist=(self.twist, DistanceInch),
+            twist_direction=TwistRight if self.rightTwist else TwistLeft,
+            sight_height=(self.sh, DistanceMillimeter),
+        )
 
-    @property
     def sound_speed(self):
-        atmosphere = Atmosphere.create_icao(self.profile._altitude)
+        atmosphere = IcaoAtmosphere(Distance(0, DistanceMeter))
         density, mach = atmosphere.get_density_factor_and_mach_for_altitude(0)
-        return unit.Velocity(mach, unit.VelocityFPS)
+        return Velocity(mach, VelocityFPS)
 
     def calculate_trajectory(self):
         self.setProfile()
@@ -116,13 +126,13 @@ class DragEditorState(State):
             return drop
         return None
 
-    def _trajectory_by_distance_filter(self, point: TrajectoryData, distance: unit.Distance):
+    def _trajectory_by_distance_filter(self, point: TrajectoryData, distance: Distance):
         if 0 <= math.fabs(point.travelled_distance.v - distance.v) <= self.profile.distance_step.v / 2:
             return point
         else:
             return None
 
-    def get_trajectory_data_at_distance(self, distance: unit.Distance):
+    def get_trajectory_data_at_distance(self, distance: Distance):
         if self.profile.trajectory_data:
             points = filter(lambda p: self._trajectory_by_distance_filter(p, distance), self.profile.trajectory_data)
             points_li = list(points)
